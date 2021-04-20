9d01f95a01b4f55dbd5d28a6e372ac9b24bce367
smkh-pro <syedadaniyahaider@gmail.com>
2021-04-20 21:36:45 +0500

    updated

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-var addimage = document.getElementById("addImage")
+var addimagecont = document.getElementById("addImagecont")
 
 function resizeDiv(){
-
-addimage.style.paddingTop=0
-addimage.style.paddingTop=addimage.offsetHeight/2+"px"
+    var offHeight =addimagecont.offsetHeight
+addimagecont.style.backgroundSize = addimagecont.offsetWidth+"px "+offHeight+"px";
+addimagecont.style.paddingTop=0
+addimagecont.style.paddingTop=offHeight/2+"px"
 }
 resizeDiv()
+
 
 window.onresize=()=>{resizeDiv()}
