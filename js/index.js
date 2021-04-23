63e8f6eb843a0bef6b32ad8d55f276e6f3082fb3
daniya-pro <syedadaniyahaider@gmail.com>
2021-04-24 01:23:19 +0500

    updated

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ var addimagecont = document.getElementById("addImagecont")
 
 function resizeDiv(){
     var offHeight =addimagecont.offsetHeight
-addimagecont.style.backgroundSize = addimagecont.offsetWidth+"px "+offHeight+"px";
 addimagecont.style.paddingTop=0
 addimagecont.style.paddingTop=offHeight/2+"px"
 }
