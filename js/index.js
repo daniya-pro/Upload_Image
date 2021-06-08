afe71e81d293f4d55820eea63c64f1062b31a44f
daniya-pro <syedadaniyahaider@gmail.com>
2021-06-09 02:16:39 +0500

    update

--- js/index.js
+++ js/index.js
@@ -1,18 +1,8 @@
-var cl = new cloudinary.Cloudinary({cloud_name: "demo", secure: true});
-var addimagecont = document.getElementById("addImagecont")
-
-function resizeDiv(){
-    var offHeight =addimagecont.offsetHeight
-addimagecont.style.paddingTop=0
-addimagecont.style.paddingTop=offHeight/2+"px"
-}
-resizeDiv()
 
 
-window.onresize=()=>{resizeDiv()}
-
-
-
+window.onresize = () => {
+  resizeDiv();
+};
 
 const url = "https://api.cloudinary.com/v1_1/dthynvvrp/image/upload";
 /**
@@ -28,10 +18,35 @@ const url = "https://api.cloudinary.com/v1_1/dthynvvrp/image/upload";
 
 
   **/
-  const button = document.getElementById("button")
-  button.addEventListener("click",UploadImg)
-var uploadImage=()=>{
+let button = document.querySelector("[type=file]");
+console.log(button);
 
-
+var UploadImage = () => {
+  var files = document.querySelector("[type=file]").files;
   
-}
+  var formData = new FormData();
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    formData.append("file", file);
+    formData.append("upload_preset", "dvjy4hua");
+    formData.append("api_key", "449178244385818");
+    
+  }
+  fetch(url, {
+    method: "POST",
+    body: formData,
+  })
+    .then((obj) => {
+      return obj.json();
+    })
+    .then((data) => {
+      var AddImage = document.getElementById("addImage");
+AddImage.innerHTML+=`<img class='image' src= ${data.url}>`
+console.log(data)
+
+    })
+    .catch((e) => {
+      alert();
+    });
+};
+button.addEventListener("change", UploadImage);
